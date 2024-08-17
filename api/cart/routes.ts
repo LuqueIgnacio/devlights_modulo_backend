5c1fd54ace6db572b1d782772ac3420cab4091a3
LuqueIgnacio <ignacioluque.micha@gmail.com>
2024-08-17 19:41:41 -0300

    Add controller, dao, model, service and routes files for Cart. Also add references to User in Product Schema

---
+++ api/cart/routes.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import cartController from "./controller";
+
+const cartRouter = express.Router()
+
+export default cartRouter
--- api/product/model.ts
+++ api/product/model.ts
@@ -26,6 +26,11 @@ const productSchema = new Schema({
     type: String,
     default: "",
   },
+  salers_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
 });
 
 const Product = model("Product", productSchema);
