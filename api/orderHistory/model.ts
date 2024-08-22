24be218ad63c9c258f40bca13fdb1636cf929391
LuqueIgnacio <ignacioluque.micha@gmail.com>
2024-08-22 20:34:20 -0300

    Modify OrderHistory Model

--- api/orderHistory/model.ts
+++ api/orderHistory/model.ts
@@ -1,25 +1,36 @@
 import { Schema, model } from "mongoose";
 
 const orderHistorySchema = new Schema({
-  date: {
-    type: Date,
-    default: Date.now,
-  },
   user_id: {
     type: Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
-  carts: [
+  products: [
     {
-      cart: {
+      product_id: {
         type: Schema.Types.ObjectId,
-        ref: "Cart",
-        required: true,
+        ref: "Product",
+        required: true
       },
+      quantity: {
+        type: Number,
+        required: true,
+        default: 1
+      },
+      sub_total:{
+        type: Number,
+        required: true
+      }
     },
   ],
-});
+  total_price:{
+    type: Number,
+    required: true
+  }
+  }, 
+  {timestamps: true}
+);
 
 const OrderHistory = model("OrderHistory", orderHistorySchema);
 
